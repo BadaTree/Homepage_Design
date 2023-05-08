202ab7095d1bf65abed5ae396f3fe8a7caffe6f6
BadaTree <zzzxxx3166@naver.com>
2023-05-08 20:11:24 +0900

    temp_update

--- homepage1/start.py
+++ homepage1/start.py
@@ -1,23 +1,11 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/hello") # 127.0.0.1
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route("/pro") #127.0.0.1
-def index1():
-    return send_from_directory('html_professos', 'index.html') # 127.0.0.1/html/css/b.css
-
-@app.route('/pro<path:name>') #127.0.0.1/css/b.css
-def start1(name):
-    return send_from_directory('html_professor',name) # 127.0.0.1/html/css/b.css"
-
-@app.route("/") #127.0.0.1
+@app.route('/')
 def index():
-    return send_from_directory('./main/html', 'index.html') # 127.0.0.1/html/index.html
+    return render_template('index.html')
 
-@app.route('/<path:name>') #127.0.0.1/css/b.css
-def start(name):
-    return send_from_directory('.main/html',name) # 127.0.0.1/html/css/b.css
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
